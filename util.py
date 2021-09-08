5ed3623aaba9afc7f82843c7b32b216b66f2499f
Kyveli-tsioli <73200902+Kyveli-tsioli@users.noreply.github.com>
2021-09-08 17:01:30 +0300

    Add files via upload

--- util.py
+++ util.py
@@ -1,25 +1,10 @@
-import logging
-import os.path
+import torch.nn as nn
+from torch.nn import init
 
-import requests
-
-logger = logging.getLogger(__name__)
-
-
-def maybe_download(url, dest):
-    """Download the url to dest if necessary, optionally checking file
-    integrity.
-    """
-    if not os.path.exists(dest):
-        logger.info('Downloading %s to %s', url, dest)
-        download(url, dest)
-
-
-def download(url, dest):
-    """Download the url to dest, overwriting dest if it already exists."""
-    response = requests.get(url, stream=True)
-    with open(dest, 'wb') as f:
-        for chunk in response.iter_content(chunk_size=1024):
-            if chunk:
-                f.write(chunk)
-
+def init_weights(obj):
+    for m in obj.modules():
+        if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+            init.xavier_normal_(m.weight)
+            m.bias.data.zero_()
+        elif isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.BatchNorm1d):
+            m.reset_parameters()
